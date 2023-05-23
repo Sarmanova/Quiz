621f6a68b1d5d70118617621a48bd2ff601db0c1
Zhansaya Sarmanova <zhansayasarmanova1993@gmail.com>
2023-05-22 22:09:29 -0500

    created a new object file

--- object.js
+++ object.js
@@ -1,30 +1,61 @@
 class Question {
-  constructor(questions, answers, choice) {
+  constructor(questions, answers, correctAnswer) {
     this.questions = questions;
     this.answers = answers;
-    this.choice = choice;
+    this.correctAnswer = correctAnswer;
   }
   showQuestionQuiz() {
+    let numbers = 0;
     console.log(`${this.questions}`);
-    this.answers.forEach((answer, index) => {
-      console.log(`${index + 1}. ${answer}`);
+    let questionNbr = numbers + 1;
+    document.querySelector(
+      "#question"
+    ).innerHTML = `${questionNbr}.  ${this.questions}`;
+
+    while (document.querySelector("#answer-buttons").firstChild) {
+      document
+        .querySelector("#answer-buttons")
+        .removeChild(document.querySelector("#answer-buttons").firstChild);
+    }
+
+    this.answers.forEach((answer) => {
+      const button = document.createElement("button");
+      button.innerHTML = answer;
+      button.classList.add("btn");
+      document.querySelector("#answer-buttons").appendChild(button);
+      document.querySelector("#nextButton").getElementsByClassName.display =
+        "none";
     });
   }
 
   correntAnswer(userChoice) {
     if (userChoice === this.choice) {
-      console.log(`${this.choice} is Correct!`);
+      console.log(`Correct!`);
     } else {
-      console.log(`${this.choice} Incorrect!`);
+      console.log(`${this.answer} Incorrect!`);
     }
   }
 }
 
-const quizQuestions = new Question(
+const questionOne = new Question(
   "How to refresh the browser on keyboard?",
   ["Ctrl F5", "Ctrl F7", "Ctrl F8", "Ctrl F9"],
   1
 );
-
-quizQuestions.showQuestionQuiz();
-quizQuestions.correntAnswer(1);
+const questionTwo = new Question(
+  "What country has the highest life expectancy?",
+  ["USA", "Canada", "Spain", "Hong Kong"],
+  4
+);
+const questionFour = new Question(
+  "What country has won the most World Cups?",
+  ["Germany", "Brazil", "Argintina", "Russia"],
+  2
+);
+const questionFive = new Question(
+  "How many minutes are in a full week?",
+  ["10,081", "10,098", "10,080", "10,090"],
+  3
+);
+questionOne.showQuestionQuiz();
+//questionOne.correntAnswer(1);
